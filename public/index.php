33a39f587587517e23d3c5eeaa65ee395b173536
Jason Lotito <jasonlotito@gmail.com>
2012-09-08 11:38:57 -0400

    Update with config and front controller

--- public/index.php
+++ public/index.php
@@ -1,12 +1,22 @@
 <?php
 namespace Funk;
 
+foreach ( $_SERVER as $key => $value )
+{
+    if ( stristr( $key, 'STEEL' ) )
+    {
+        echo "$key : $value <br>";
+    }
+}
+
+define('CONFIG_FILE', __DIR__ . '/../config.xml');
+
 // It's probably best that Steel is added to the path in PHP.  If this isn't possible, you can set it
 // by calling setIncludePath().  You can also add in additional include paths.
 require '../Steel/Bootstrap.php';
 
 // Steel\Bootstrap assumes that it's location is in the base directory of Steel.
-$bootstrap = \Steel\Bootstrap::init()->setIncludePath();
+$bootstrap = \Steel\Bootstrap::init();
+echo get_include_path();
 
-
-\Steel\Application::start()->run();
+\Steel\Application::start(CONFIG_FILE)->run();
