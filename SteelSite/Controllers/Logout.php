682b894b9ec082a7381be4179cd64cc0a4cf16a5
Jason Lotito <jasonlotito@gmail.com>
2012-09-12 22:31:52 -0400

    Routing works, /logout works as expected

---
+++ SteelSite/Controllers/Logout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SteelSite\Controllers;
+
+/**
+ * Logout
+ *
+ * ${DESCRIPTION}
+ *
+ * @module ${MODULE}
+ * @submodule ${SUBMODULE}
+ * @author Jason Lotito <jasonlotito@gmail.com>
+ */
+class Logout
+{
+    public function get()
+    {
+        echo "This controller works!";
+    }
+}
